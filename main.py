d76db4519665788c27c04de9517f2f36c288544e
john <97820338+johm01@users.noreply.github.com>
2023-01-10 01:50:49 -0600

    styling

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ class App:
         # Meter Frame 
         self.f1 = tk.Frame(self.root)
         self.f1.place(x=150,y=150)
-        self.m2 = tk.Meter(self.f1,amounttotal=110,amountused=0,metersize=110,stepsize=10)
+        self.m2 = tk.Meter(self.f1,amounttotal=110,amountused=0,metersize=150,stepsize=10,subtext='Temp F')
         self.m2.grid(column=1,row=1)
-        self.m3 = tk.Meter(self.f1,amounttotal=100,amountused=0,metersize=110,stepsize=10)
+        self.m3 = tk.Meter(self.f1,amounttotal=100,amountused=0,metersize=150,stepsize=10,subtext='Humidity',showtext=True)
         self.m3.grid(column=0,row=1)
-        self.m4 = tk.Meter(self.f1,amounttotal=100,amountused=0,metersize=110,stepsize=10)
+        self.m4 = tk.Meter(self.f1,amounttotal=100,amountused=0,metersize=150,stepsize=10,subtext='Wind Speed',showtext=True)
         self.m4.grid(column=0,row=2)
-        self.m5 = tk.Meter(self.f1,amounttotal=100,amountused=0,metersize=110,stepsize=10)
+        self.m5 = tk.Meter(self.f1,amounttotal=100,amountused=0,metersize=150,stepsize=10,subtext='Temp C')
         self.m5.grid(column=1,row=2)
         
         # Entry and Button frame
@@ -38,6 +38,7 @@ class App:
         self.l2 = tk.Label(self.f3,font=('Terminus (TTF)',20),style="success.TLabel")
         self.l2.pack()
         self.l3 = tk.Label(self.f3,font=('Terminus (TTF)',10),style="success.TLabel")
+        self.l3.pack()
         self.root.mainloop()
 
     def get_weather(self,zip):
@@ -45,21 +46,22 @@ class App:
         key = f'http://api.weatherapi.com/v1/current.json?key=9882b06f04a14ed49fd02510222704&q={zip}&aqi=no'
             
         # Making a request to the weather API
-        r = requests.get(key)
         try:
+            r = requests.get(key)
             data = r.json()
+            print(data)
             if data:
-                current = data['current']
-                location = data['location']
-                condition = data['condition']
                 try:
+                    current = data['current']
+                    location = data['location']
+                    
                     self.m2['amountused'] = int(current['temp_f'])
                     self.m3['amountused'] = int(current['humidity'])
                     self.m4['amountused'] = int(current['wind_mph'])
                     self.m5['amountused'] = int(current['temp_c'])
                     self.l1['text'] = location['name'] +', '+ location['region']
                     self.l2['text'] = 'Feels like ' + str(int(current['feelslike_f']))
-                    self.l3['text'] = condition['text']
+                    self.l3['text'] = current['condition']['text']
                 except:
                     print('Error: '+data['error']['code'])
         except:
